bba2d15fc13a187282ad0343299b524cdb083899
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-08-28 15:55:09 -0700

    Rewrote deposit point logic and cleaned up the script

--- Game/entities/objects/deposit_point.gd
+++ Game/entities/objects/deposit_point.gd
@@ -15,57 +15,47 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func deposit():
-	match deposit_type:
-		accepts.TRASH:
-			#if MissionInventory.level_trash_count > 0:
-				#MissionInventory.remove_item(true)
-				#GameManager.mission_add_score.emit(1)
-			pass
-		accepts.RECYCLE:
-			#if MissionInventory.level_recycle_count > 0:
-				#MissionInventory.remove_item(false)
-				#GameManager.mission_add_score.emit(1)
-			pass
 	
 func deposit_from_pocket(leftright : bool):
-	if !leftright:
-		match deposit_type:
-			accepts.TRASH:
-				if PocketManager.left_pocket_recycle >= 1:
-					mission.add_wrong_deposit(PocketManager.left_pocket_recycle)
-				else:
-					mission.complete_objective(PocketManager.left_pocket_current)
-			accepts.RECYCLE:
-				if PocketManager.left_pocket_trash >= 1:
-					mission.add_wrong_deposit(PocketManager.left_pocket_trash)
-				else:
-					mission.complete_objective(PocketManager.left_pocket_current)
-		
-		#GameManager.mission_add_score.emit(PocketManager.left_pocket_current)	
-	else:
-		match deposit_type:
-			accepts.TRASH:
-				if PocketManager.right_pocket_recycle >= 1:
-					mission.add_wrong_deposit(PocketManager.right_pocket_recycle)
-				else:
-					mission.complete_objective(PocketManager.right_pocket_current)
-			accepts.RECYCLE:
-				if PocketManager.left_pocket_trash >= 1:
-					mission.add_wrong_deposit(PocketManager.right_pocket_trash)
-				else:
-					mission.complete_objective(PocketManager.right_pocket_current)
-		#GameManager.mission_add_score.emit(PocketManager.right_pocket_current)
-	
-	PocketManager.empty_pocket(leftright)
+	# Okay the flow for this is kinda fucked and could DEFINITELY be done better but it works for now
+	# Its a lot of repeated logic
+	match deposit_type:
+		accepts.TRASH:
+			if !leftright:
+				# Left Pocket
+				if PocketManager.left_pocket_current >= 1:
+					if PocketManager.left_pocket_mixed:
+						mission.add_wrong_deposit(PocketManager.left_pocket_recycle)
+					else:
+						mission.complete_objective(PocketManager.left_pocket_trash)
+					PocketManager.empty_pocket(leftright)
+			else:
+				# Right Pocket
+				if PocketManager.right_pocket_current >= 1:
+					if PocketManager.right_pocket_mixed:
+						mission.add_wrong_deposit(PocketManager.right_pocket_recycle)
+						pass
+					else:
+						mission.complete_objective(PocketManager.right_pocket_trash)
+					PocketManager.empty_pocket(leftright)
+		accepts.RECYCLE:
+			if !leftright:
+				# Left Pocket
+				if PocketManager.left_pocket_current >= 1:
+					if PocketManager.left_pocket_mixed:
+						mission.add_wrong_deposit(PocketManager.left_pocket_trash)
+					else:
+						mission.complete_objective(PocketManager.left_pocket_recycle)
+					PocketManager.empty_pocket(leftright)
+			else:
+				# Right Pocket
+				if PocketManager.right_pocket_current >= 1:
+					if PocketManager.right_pocket_mixed:
+						mission.add_wrong_deposit(PocketManager.right_pocket_trash)
+					else:
+						mission.complete_objective(PocketManager.right_pocket_recycle)
+					PocketManager.empty_pocket(leftright)
+
 	GameManager.ui_update_score_count.emit(mission)
 	GameManager.ui_update_item_counts.emit()
-	
-#func deposit_both():
-#	if is_infinite:
-#		var sum : int = MissionInventory.get_sum()
-#		GameManager.mission_add_score.emit(sum)
-#		MissionInventory.remove_garbage(MissionInventory.level_trash_count)
-#		GameManager.ui_update_score_count.emit()
 		
