0bf5ef7ac19dae4afc42e3a43b4c86e0a38a0272
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-08-29 14:10:23 -0700

    Made the grabbing function disable the collision instead of deleting the component

--- Game/player/tools/tool_hands.gd
+++ Game/player/tools/tool_hands.gd
@@ -3,6 +3,7 @@ extends PlayerTool
 var holding : bool = false
 @export var hold_point : Node3D
 var held_object : Node3D
+var held_object_interact : InteractComponent
 var grab_speed
 
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +16,7 @@ func _tool_primary() -> void:
 		if frobber.col_to_select && is_instance_valid(frobber.col_to_select):
 			if frobber.col_to_select.is_in_group("grabbable"):
 				print("grabbed")
+				held_object_interact = frobber.col_to_select
 				var grabbed_obj = frobber.col_to_select.get_parent()
 				held_object = grabbed_obj
 				grabbed_obj = null
@@ -22,7 +24,7 @@ func _tool_primary() -> void:
 				held_object.reparent(hold_point)
 				held_object.position = hold_point.position
 				held_object.rotation = hold_point.rotation
-				frobber.col_to_select.queue_free()
+				held_object_interact.collision.disabled
 				holding = true
 				
 func _tool_secondary() -> void:
@@ -34,5 +36,7 @@ func _tool_secondary() -> void:
 			var angle = manager.player.global_rotation.y
 			held_object.apply_central_impulse(-manager.player.global_transform.basis.z.normalized() * 15 + Vector3(0,5,0))
 			held_object.reparent(manager.player.get_parent())
+			held_object_interact.collision.disabled = false
 			held_object = null
+			held_object_interact = null
 			holding = false
