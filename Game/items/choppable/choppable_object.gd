5bdddcae0350ad551dcfebe9296fcc478277ee36
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-08-29 14:12:06 -0700

    Defined logic for choppable objects and chopped items

---
+++ Game/items/choppable/choppable_object.gd
@@ -0,0 +1,46 @@
+extends Area3D
+
+@export var center_segment : ChoppedObject
+@export var left_segment : ChoppedObject
+@export var right_segment : ChoppedObject
+
+@export var left_interact : InteractComponent
+@export var right_interact : InteractComponent
+
+var left_chopped : bool = false
+var right_chopped : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	center_segment.disable_process()
+	left_segment.disable_process()
+	right_segment.disable_process()
+	get_parent().get_parent().register_new_objective(3)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+
+func generic_chop():
+	if left_chopped && right_chopped:
+		center_segment.reparent(get_parent().get_parent())
+		center_segment.enable_process()
+func on_chop_left():
+	print("chopped left")
+	left_segment.reparent(get_parent().get_parent())
+	left_segment.enable_process()
+	left_interact.queue_free()
+	left_chopped = true
+	generic_chop()
+	pass
+
+func on_chop_right():
+	print("chopped right")
+	right_segment.reparent(get_parent().get_parent())
+	right_segment.enable_process()
+	right_interact.queue_free()
+	right_chopped = true
+	generic_chop()
+	pass
---
+++ Game/items/choppable/chopped_object.gd
@@ -0,0 +1,27 @@
+extends RigidBody3D
+
+class_name ChoppedObject
+@export var interact_component : InteractComponent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func disable_process():
+	freeze = true
+	print("disabled process")
+	interact_component.set_process(false)
+	interact_component.visible = false
+	interact_component.collision.disabled = true
+
+func enable_process():
+	freeze = false
+	interact_component.set_process(true)
+	interact_component.visible = true
+	interact_component.collision.disabled = false
+	
