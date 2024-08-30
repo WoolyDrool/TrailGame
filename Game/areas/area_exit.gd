35626ce57ec627a92008e1355f1e7380a6fc6d53
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-08-29 21:28:14 -0700

    Made a basic area exit trigger

--- Game/areas/area_exit.gd
+++ Game/areas/area_exit.gd
@@ -12,3 +12,8 @@ func _process(delta: float) -> void:
 
 func raise_gate():
 	$AnimationPlayer.play("area_complete_raisebarrier")
+
+
+func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body is Player:
+		GameManager.load_scene.emit("res://Content/maps/level_1.tscn")
