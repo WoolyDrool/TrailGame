5b2a6fed092e0b55b8f5c1079ef00b6d95324c6d
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-08-31 07:59:16 -0700

    Added health to each choppable side so the player has to hit it multiple times

--- Game/items/choppable/choppable_object.gd
+++ Game/items/choppable/choppable_object.gd
@@ -8,7 +8,9 @@ extends Area3D
 @export var right_interact : InteractComponent
 
 var left_chopped : bool = false
+var left_health : = 3
 var right_chopped : bool = false
+var right_health : = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,23 +26,28 @@ func _process(delta: float) -> void:
 	pass
 
 func generic_chop():
+	print(left_health, left_chopped, right_health, right_chopped)
 	if left_chopped && right_chopped:
 		center_segment.reparent(get_parent().get_parent())
 		center_segment.enable_process()
 func on_chop_left():
 	print("chopped left")
-	left_segment.reparent(get_parent().get_parent())
-	left_segment.enable_process()
-	left_interact.queue_free()
-	left_chopped = true
+	left_health -= 1
+	if left_health <= 0:
+		left_segment.reparent(get_parent().get_parent())
+		left_segment.enable_process()
+		left_interact.queue_free()
+		left_chopped = true
 	generic_chop()
 	pass
 
 func on_chop_right():
 	print("chopped right")
-	right_segment.reparent(get_parent().get_parent())
-	right_segment.enable_process()
-	right_interact.queue_free()
-	right_chopped = true
+	right_health -= 1
+	if right_health <= 0:
+		right_segment.reparent(get_parent().get_parent())
+		right_segment.enable_process()
+		right_interact.queue_free()
+		right_chopped = true
 	generic_chop()
 	pass
