2731689fccaad1cef69ab80307a62b3816f9f7b8
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-08-31 07:00:01 -0700

    Refined pocketing logic (again)

--- Game/items/objective_item.gd
+++ Game/items/objective_item.gd
@@ -17,13 +17,17 @@ func _ready():
 func on_pocket(leftright : bool):
 	if type != item_type.NONE:
 		if !leftright:
-			if PocketManager.left_pocket_current <= PocketManager.max_pocket_size:
+			if PocketManager.left_pocket_current < PocketManager.max_pocket_size:
 					PocketManager.add_to_pockets(self, leftright)
 					queue_free()
+			else:
+				print("left pocket full")
 		else:
-			if PocketManager.right_pocket_current <= PocketManager.max_pocket_size:
+			if PocketManager.right_pocket_current < PocketManager.max_pocket_size:
 					PocketManager.add_to_pockets(self, leftright)
 					queue_free()
+			else:
+				print("right pocket full")
 	else:
 		print("Item does not have a type")
 	GameManager.ui_update_item_counts.emit()
