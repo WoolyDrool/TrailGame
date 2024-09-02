df6970ec72a45bd4668a15a6508256dd3bdb4bc9
WoolyDrool <nikolai.mcneely15@gmail.com>
2024-09-02 13:45:45 -0700

    Gave objective items the ability to disable their interacts similarly to chopped objects

--- Game/items/objective_item.gd
+++ Game/items/objective_item.gd
@@ -7,12 +7,24 @@ class_name ObjectiveItem
 enum item_type {NONE, TRASH, RECYCLE, KEY}
 @export var type : item_type
 @export var pocketable : bool = true
+@export var interact_component : InteractComponent
 
 func _ready():
 	if !type == item_type.KEY:
 		get_parent().get_parent().register_new_objective(objective_worth)
 	
 	$Interact.interactText = item_name
+
+func disable_process():
+	interact_component.set_process(false)
+	interact_component.visible = false
+	interact_component.collision.disabled = true
+
+func enable_process(): 
+	interact_component.set_process(true)
+	interact_component.visible = true
+	interact_component.collision.disabled = false
+	
 
 func on_pocket(leftright : bool):
 	if type != item_type.NONE:
