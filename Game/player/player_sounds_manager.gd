ec8ac046984beb17cbd37cdb703737976c1d28f2
WoolyDrool <nikolai.mcneely15@gmail.com>
2025-09-29 08:27:17 -0700

    Started basic work on some audio stuff9

--- Game/player/player_sounds_manager.gd
+++ Game/player/player_sounds_manager.gd
@@ -1,7 +1,10 @@
-class_name GameAudioManager
-extends AudioStreamPlayer
+class_name PlayerSoundManager
+extends Node3D
 
-var global_player = "res://App/global_audio_player.tscn"
+@export_category("Tracks")
+@export var footsteps_track : AudioStreamPlayer3D
+@export var barks_track : AudioStreamPlayer3D
+@export var tools_track : AudioStreamPlayer3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,9 +13,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func play_sound(clip : AudioStream):
-	var new_clip = load(global_player)
-	new_clip.stream = clip
 	pass
