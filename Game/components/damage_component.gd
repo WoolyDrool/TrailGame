24a4da92ee5cf89bb420a6a314750dd79083af4e
WoolyDrool <nikolai.mcneely15@gmail.com>
2025-09-29 08:25:53 -0700

    Cleaned damage component functionality

--- Game/components/damage_component.gd
+++ Game/components/damage_component.gd
@@ -1,5 +1,4 @@
 extends Area3D
-var fired : bool = false
 @export var damage_to_deal : int = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -14,8 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		if !fired:
-			GameManager.player_take_damage.emit(damage_to_deal)
-			fired = true
-		else:
-			return
+		GameManager.player_take_damage.emit(damage_to_deal)
